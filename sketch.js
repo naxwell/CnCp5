9e7ea6c1e5cd510ffd69a1ba3cdfe703d0cc9909
Glitch (light-octopus) <none>
2017-10-22 23:32:31 +0000

    🎅🍚 Updated with Glitch - working eraser + colour

--- sketch.js
+++ sketch.js
@@ -4,30 +4,22 @@ function setup()
 {
   createCanvas(windowWidth, windowHeight);
   img = loadImage("assets/heart.jpg");
-
-  background(img);
   stroke(255);
   noFill();
 }
 
 function draw() 
 {
-	
-	//image(img, 0,0);
-	//background(120);
-	//if(mouseIsPressed){
-	//	noFill();
-	//	ellipse(mouseX,mouseY,200,200);
-	//}	
+  //image(img,0,0,width,height); //displays full screen image
 
-   if(mouseIsPressed){
-   	copy(img, mouseX, mouseY, 100, 100, mouseX, mouseY, 100, 100);
+  if(mouseIsPressed){
+    copy(img, mouseX, mouseY, 100, 100, mouseX, mouseY, 100, 100);
 }
 
-  //var pointillize = map(random, 0, width, 2, 18);
+  
   var x = int(random(width));
   var y = int(random(height));
-  fill(255);
+  fill(244, 200, 66);
   noStroke()
   ellipse(x, y, 100, 100);
 
