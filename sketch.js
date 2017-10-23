35c08de4abcb229566883a46760b7dfec0cc9ec5
Glitch (light-octopus) <none>
2017-10-23 16:38:20 +0000

    👨‍❤️‍👨👹 Shake to make it stop!

--- sketch.js
+++ sketch.js
@@ -20,22 +20,30 @@ function getRandomImage(imgAr, path) {
 //var img
 var images 
 var imgNumber
+var onMode
 
 function setup() 
 {
   createCanvas(windowWidth, windowHeight);
-  img1 = loadImage("https://cdn.glitch.com/2cb70ef7-6218-4339-81cd-a524df4eae38%2Fclean8.jpg?1508715563223");
-  img2 = loadImage("https://cdn.glitch.com/2cb70ef7-6218-4339-81cd-a524df4eae38%2Fclean.jpg?1508715562852");
-  img3 = loadImage("https://cdn.glitch.com/2cb70ef7-6218-4339-81cd-a524df4eae38%2Fclean3.jpg?1508715562982");
-  img4 = loadImage("https://cdn.glitch.com/2cb70ef7-6218-4339-81cd-a524df4eae38%2Fclean7.jpg?1508715563024");
+  onMode = true
+  img1 = loadImage("https://cdn.glitch.com/2cb70ef7-6218-4339-81cd-a524df4eae38%2Fbig-006.jpg?1508775846954");
+  img2 = loadImage("https://cdn.glitch.com/2cb70ef7-6218-4339-81cd-a524df4eae38%2Fbig-017.jpg?1508775847016");
+  img3 = loadImage("https://cdn.glitch.com/2cb70ef7-6218-4339-81cd-a524df4eae38%2Fbig-008.jpg?1508775847076");
+  img4 = loadImage("https://cdn.glitch.com/2cb70ef7-6218-4339-81cd-a524df4eae38%2Fbig-002.jpg?1508775847452");
   images = [img1, img2, img3, img4];
   imgNumber = random([0,1,2,3]);
-  //console.log(imgNumber)
+  console.log(imgNumber)
   button = createButton('GIMME A NEW IMAGE');
-  button.position(100,100);
+  button.position(width/2-50,100);
   button.mousePressed(nextImage);
   background(244, 200, 66);
+  //button = createButton('KEEP IMAGE');
+  //button.position (width/2-50,height/3);
+  //button.mousePressed(stahp);
   
+}
+function deviceShaken() {
+  onMode = false;
 }
 
 function nextImage(){
@@ -45,6 +53,7 @@ function nextImage(){
   else {
   imgNumber++;
   }
+  onMode = true;
   background(244, 200, 66);
 }
 function mouseDragged(){
@@ -59,15 +68,15 @@ function draw()
     //copy(images[imgNumber], mouseX, mouseY, 100, 100, mouseX, mouseY, 100, 100);
 //}
 
-  
+if (onMode == true){
   var x = int(random(width));
   var y = int(random(height));
   fill(244, 200, 66);
   noStroke()
   ellipse(x, y, 100, 100);
-
 }
 
+}
 
 
 function windowResized() 
