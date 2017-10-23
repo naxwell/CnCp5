6e10603cae7051dee82227028c216c6ea7ccf079
Glitch (light-octopus) <none>
2017-10-23 13:29:28 +0000

    🚊🐙 Working random image puller

--- sketch.js
+++ sketch.js
@@ -1,20 +1,63 @@
-var img
+
+/* This was the random image puller I had on file from an old website that was used for reference
+  var random_images_array = [
+  "2cb70ef7-6218-4339-81cd-a524df4eae38%2Fclean.jpg?1508715562852", 
+  "2cb70ef7-6218-4339-81cd-a524df4eae38%2Fclean3.jpg?1508715562982", 
+  "2cb70ef7-6218-4339-81cd-a524df4eae38%2Fclean7.jpg?1508715563024", 
+  "2cb70ef7-6218-4339-81cd-a524df4eae38%2Fclean8.jpg?1508715563223"
+  ];
+
+function getRandomImage(imgAr, path) {
+    path = path || 'https://cdn.glitch.com/'; // default path here
+    var num = Math.floor( Math.random() * imgAr.length );
+    var img = imgAr[ num ];
+    return path + imgAr[ num ];
+    //var imgStr = '<img src="' + path + img + '" alt = "">';
+    //document.write(imgStr); document.close();
+}*/
+
+//Photo stamp stuff
+//var img
+var images 
+var imgNumber
 
 function setup() 
 {
   createCanvas(windowWidth, windowHeight);
-  img = loadImage("assets/heart.jpg");
-  stroke(255);
-  noFill();
+  img1 = loadImage("https://cdn.glitch.com/2cb70ef7-6218-4339-81cd-a524df4eae38%2Fclean8.jpg?1508715563223");
+  img2 = loadImage("https://cdn.glitch.com/2cb70ef7-6218-4339-81cd-a524df4eae38%2Fclean.jpg?1508715562852");
+  img3 = loadImage("https://cdn.glitch.com/2cb70ef7-6218-4339-81cd-a524df4eae38%2Fclean3.jpg?1508715562982");
+  img4 = loadImage("https://cdn.glitch.com/2cb70ef7-6218-4339-81cd-a524df4eae38%2Fclean7.jpg?1508715563024");
+  images = [img1, img2, img3, img4];
+  imgNumber = random([0,1,2,3]);
+  //console.log(imgNumber)
+  button = createButton('GIMME A NEW IMAGE');
+  button.position(100,100);
+  button.mousePressed(nextImage);
+  background(244, 200, 66);
+  
 }
 
+function nextImage(){
+  if (imgNumber == 3){
+    imgNumber=0
+  }
+  else {
+  imgNumber++;
+  }
+  background(244, 200, 66);
+}
+function mouseDragged(){
+  copy(images[imgNumber], mouseX, mouseY, 200, 200, mouseX, mouseY, 200, 200);
+}
 function draw() 
 {
-  //image(img,0,0,width,height); //displays full screen image
-
-  if(mouseIsPressed){
-    copy(img, mouseX, mouseY, 100, 100, mouseX, mouseY, 100, 100);
-}
+  
+  //image(images[2],0,0,width,height); //displays full screen image
+  // original copy code
+  //if(mouseIsPressed){ 
+    //copy(images[imgNumber], mouseX, mouseY, 100, 100, mouseX, mouseY, 100, 100);
+//}
 
   
   var x = int(random(width));
@@ -31,3 +74,4 @@ function windowResized()
 {
   resizeCanvas(windowWidth, windowHeight);
 }
+
