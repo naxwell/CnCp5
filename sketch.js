d9aa7d6cdda8cbc3f21ae91f8e4ad6e983753bda
Glitch (light-octopus) <none>
2017-10-26 16:54:42 +0000

    🏔🐸 Final with Comments!

--- sketch.js
+++ sketch.js
@@ -1,4 +1,10 @@
-//var img
+/* 
+The Dough Not Game
+by Savaya Shinkaruk + Max Lander
+for Creation & Computation DIGF-6037-001 at OCAD U
+*/
+
+
 var images 
 var imgNumber
 var onMode
@@ -6,8 +12,8 @@ var onMode
 function setup() 
 {
   createCanvas(windowWidth, windowHeight); 
-  onMode = true
-  img1 = loadImage("https://cdn.glitch.com/2cb70ef7-6218-4339-81cd-a524df4eae38%2Fdough_02.jpg?1509032640166");
+  onMode = true //used to initiate ellipse draw function
+  img1 = loadImage("https://cdn.glitch.com/2cb70ef7-6218-4339-81cd-a524df4eae38%2Fdough_02.jpg?1509032640166"); //load all images 
   img2 = loadImage("https://cdn.glitch.com/2cb70ef7-6218-4339-81cd-a524df4eae38%2Fdough_01.jpg?1509032640233");
   img3 = loadImage("https://cdn.glitch.com/2cb70ef7-6218-4339-81cd-a524df4eae38%2Fdough_06.jpg?1509032640241");
   img4 = loadImage("https://cdn.glitch.com/2cb70ef7-6218-4339-81cd-a524df4eae38%2Fdough_07.jpg?1509032640350");
@@ -27,26 +33,23 @@ function setup()
   img18 = loadImage("https://cdn.glitch.com/2cb70ef7-6218-4339-81cd-a524df4eae38%2Fdough_20.jpg?1509032641749");
   img19 = loadImage("https://cdn.glitch.com/2cb70ef7-6218-4339-81cd-a524df4eae38%2Fdough_19.jpg?1509032642073");
   img20 = loadImage("https://cdn.glitch.com/2cb70ef7-6218-4339-81cd-a524df4eae38%2Fdough_14.jpg?1509032642428");
-  images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20];
-  imgNumber = random([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]);
+  images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20]; //creates array of images for copy to call on later on
+  imgNumber = random([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]); //loads a random number which will be linked to an image in copy command later on
   
-  button = createButton('');
+  button = createButton('');       // create an empty button - all button visual settings in css
   button.position(width/2-50,100);
-  button.mousePressed(nextImage);
-  background('#fddc4d');
-  //button = createButton('KEEP IMAGE');
-  //button.position (width/2-50,height/3);
-  //button.mousePressed(stahp);
+  button.mousePressed(nextImage);  //titles the funtion to be run whe nthe button is pressed
+  background('#fddc4d');           //yellow backgound
   
 }
-function deviceShaken() {
-  onMode = false;
+function deviceShaken() { //this initiates the final stage of the puzzle 
+  onMode = false;         //when device is shaken the ellipses stop spawning and the button for a new image is no longer visible
   button.hide();
 }
 
-function nextImage(){
-  if (imgNumber == 19){
-    imgNumber=0
+function nextImage(){     //the function that runs on the button being pressed
+  if (imgNumber == 19){   //when the button is pressed, load the next image in numberical order
+    imgNumber=0           //when you reach the final image, start back at the begining
   }
   else {
   imgNumber++;
@@ -55,32 +58,32 @@ function nextImage(){
   background(244, 200, 66);
 }
 
-function mouseDragged(){
-  copy(images[imgNumber], mouseX, mouseY, 200, 200, mouseX, mouseY, 200, 200);
-}
-
+function mouseDragged(){                                                       //mouseDragged worked best for finger swipes in our test
+  copy(images[imgNumber], mouseX, mouseY, 200, 200, mouseX, mouseY, 200, 200); //when finger is dragged accross screen "reveal" image at mouses (finger) location
+}                                                                              //in actuality this is using the image as a stamp to paint with
+                                                                               //on page load imgNumber is randomized so each screen get a random result
 function draw() 
 {
-  console.log(imgNumber);
+  console.log(imgNumber);          //for debugging to ensure button is clicking through numbers correctly
   
 
-if (onMode == true){
-  frameRate(2);
-  var x = int(random(width));
+if (onMode == true){               //when onMode equals true, draw a series of randomly placed ellipses. 
+  frameRate(2);                    //sets the speed for the ellipses to spawn
+  var x = int(random(width));      //generates two random numbers in the width and height of the canvas, giving us random coordinates
   var y = int(random(height));
-  fill('#fbf8f1');
+  fill('#fbf8f1');                 //off-white
   noStroke()
-  ellipse(x, y, 100, 100);
+  ellipse(x, y, 100, 100);         //draws a small ellipse at the randomized coordinates
   
   var x = int(random(width));
   var y = int(random(height));
-  fill('#c36f05');
+  fill('#c36f05');                //orange
   noStroke()
   ellipse(x, y, 200, 200);
   
   var x = int(random(width));
   var y = int(random(height));
-  fill('#311102');
+  fill('#311102');                //dark brown
   noStroke()
   ellipse(x, y, 400, 400);
 }
@@ -88,7 +91,7 @@ if (onMode == true){
 }
 
 
-function windowResized() 
+function windowResized()          //when the window is resized, resize the canvas accordingly
 {
   resizeCanvas(windowWidth, windowHeight);
 }
